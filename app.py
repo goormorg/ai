95a493771332e31386cd848c307d13150af950a2
Dipokal HHJ <hhj@devent.kr>
2024-01-28 03:41:19 +0900

    feat: add more

--- app.py
+++ app.py
@@ -56,3 +56,29 @@ def read_root(gender: str, age: str, weight: str, muscle: str, goal: str):
     return {
         "result": result
     }
+
+
+@app.get("/get/more")
+def read_root(gender: str, age: str, weight: str, muscle: str, goal: str):
+    result = ''
+
+    stream = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": """성별, 나이, 몸무게, 키, 골격근량, 목표를 입력하면, 너는 아침, 점심, 저녁에 해당하는 메뉴와 각 메뉴에 해당하는 영양소를 {{ 아침: {{ 메뉴: 메뉴배열, 영양소: {{ 칼로리, 탄수화물, 단백질, 지방, 당류, 포화지방, 나트륨, 칼슘 }} }}, 점심: {{ 메뉴: 메뉴배열, 영양소: {{ 칼로리, 탄수화물, 단백질, 지방, 당류, 포화지방, 나트륨, 칼슘 }} }}, 저녁: {{ 메뉴: 메뉴배열, 영양소: {{ 칼로리, 탄수화물, 단백질, 지방, 당류, 포화지방, 나트륨, 칼슘 }} }} }} JSON 형식으로 추천해줘.
+
+성별={gender}, 나이={age}, 몸무게={weight}, 골격근량={muscle}, 목표={goal}""".format(gender=gender,age=age,weight=weight,muscle=muscle,goal=goal),
+            }
+        ],
+        model="gpt-3.5-turbo",
+        stream=True,
+
+    )
+
+    for chunk in stream:
+        result += chunk.choices[0].delta.content or ""
+
+    return {
+        "result": result
+    }
